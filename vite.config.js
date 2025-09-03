444effcbeff995e0cc3dc938627515cb414a1296
Min <min82814@gmial.com>
2025-09-03 16:39:00 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/web-layout-training-vite/',
+  base: '/TW/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
